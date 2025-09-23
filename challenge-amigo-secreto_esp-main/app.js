9482c2bb3e01e658adbff07679c19d5287a92af9
RocioB14 <rociobolanos.t@gmail.com>
2025-09-22 21:57:01 -0600

    Crear arreglo y funcion agregar amigos

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    const textoAmigos = document.getElementById("amigo").value;
+    
+    if (textoAmigos == "" || textoAmigos == null){
+        alert("Por favor, inserte un nombre");
+               
+    }else{
+        amigos.push(textoAmigos);
+    }
+
+    document.getElementById("amigo").value = "";
+
+}
+
+
